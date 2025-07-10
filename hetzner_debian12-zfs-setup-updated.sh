0b96a8e8e6e87ae9bd1d3f28b7f5315ec2a8a502
Kai Biebel <38378574+seclution@users.noreply.github.com>
2025-07-10 09:46:34 +0200

    Update hetzner_debian12-zfs-setup-updated.sh
    aks directly for mirror

--- hetzner_debian12-zfs-setup-updated.sh
+++ hetzner_debian12-zfs-setup-updated.sh
@@ -254,6 +254,13 @@ Devices with mounted partitions, cdroms, and removable devices are not displayed
     fi
   done
 
+  pools_mirror_option=
+  if [[ ${#v_selected_disks[@]} -gt 1 ]]; then
+    if dialog --defaultno --yesno "Do you want to use mirror mode for ${v_selected_disks[*]}?" 30 100; then 
+      pools_mirror_option=mirror
+    fi
+  fi
+
   print_variables v_selected_disks
 }
 
@@ -554,13 +561,6 @@ echo "======= create zfs pools and datasets =========="
     rpool_disks_partitions+=("${selected_disk}-part3")
     bpool_disks_partitions+=("${selected_disk}-part2")
   done
-
-  pools_mirror_option=
-  if [[ ${#v_selected_disks[@]} -gt 1 ]]; then
-    if dialog --defaultno --yesno "Do you want to use mirror mode for ${v_selected_disks[*]}?" 30 100; then 
-      pools_mirror_option=mirror
-    fi
-  fi
 
 # shellcheck disable=SC2086
 echo -n "$v_passphrase" | zpool create \
